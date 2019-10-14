9835cb6e786d7596836676a81246d3e79cea9e9b
StanisLove Sid <joingamings@gmail.com>
2019-10-14 19:39:26 +0300

    respond with projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,9 @@
 class ProjectsController < ApplicationController
   # before_action :authenticate_user!
-  # before_action :set_current_user
+  before_action :set_current_user, only: %i[index]
 
   def index
-    respond_with(Project.all)
+    respond_with(@current_user.projects)
   end
 
   def create
